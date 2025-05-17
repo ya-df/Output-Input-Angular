a7abd0804716b34e8a976844e62e211cc1a79ac1
yasmine amara <amarayasmine13@gmail.com>
2025-05-17 13:31:48 +0100

    commit change 2

--- src/app/components/parent-component/parent-component.component.ts
+++ src/app/components/parent-component/parent-component.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent-component.component.css']
 })
 export class ParentComponentComponent  implements OnInit{
-  public name="Salma";
+  public name="marwa";
   public messageReceived="";
 
 
